b8e3ef46d2a065a20da548f159c437b3acccb2cb
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-09 17:42:20 -0300

    resolve requisito 5 gerando cards de produtos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
-import getCategories from './services/api';
 
-import ProductsList from './Components/ProductsList';
+import ProductsList from './pages/ProductsList';
 
 function App() {
-  getCategories();
   return (
     <div className="App">
       <BrowserRouter>
