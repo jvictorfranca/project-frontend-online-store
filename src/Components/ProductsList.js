a086118f823dcf7553d617023723c556374ae39e
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-09 15:38:37 -0300

    Resolve requisito 2, com componente ProductsList e sua renderizaçao na home /

---
+++ src/Components/ProductsList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class ProductsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searching: 'none',
+    };
+  }
+
+  render() {
+    const { state } = this;
+    const { searching } = state;
+    return (
+      <main data-testid="home-initial-message">
+        {searching === 'none'
+          ? <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+          : <p>Lista de produtos</p>}
+      </main>
+    );
+  }
+}
+
+export default ProductsList;
