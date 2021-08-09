355f708672ff4f09562b3f3ba539ae720415100c
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-09 17:42:25 -0300

    resolve requisito 5 gerando cards de produtos

---
+++ src/Components/ProductList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProductCard from './ProductCard';
+
+class ProductList extends React.Component {
+  render() {
+    const { products } = this.props;
+    return (
+      products.length === 0
+        ? <p>Nenhum produto encontrado</p>
+        : <section>
+          {products.map((product) => (<ProductCard
+            product={ product }
+            key={ product.id }
+          />))}
+
+        </section>);
+  }
+}
+
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+};
+
+export default ProductList;
