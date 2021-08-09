34ac8be4d03d85bf14633339ab9497fea130c670
iroDevs <ironegrolol@gamil.com>
2021-08-09 15:45:14 -0300

    linter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import '../src/services/api'
+import './services/api';
 
 function App() {
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ export async function getCategories() {
   fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`)
     .then((response) => response.json())
     .then((data) => console.log(data));
-
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
