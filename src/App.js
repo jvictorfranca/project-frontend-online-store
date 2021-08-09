ce9ff83b8773ebfb46dcd3a1c02e7ffc71671552
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-08-09 16:47:50 -0300

    fix bug in App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import './App.css';
-import getCategories from './services/api';
 
 import ProductsList from './Components/ProductsList';
 import ShoppingCart from './Components/ShoppingCart';
 
 function App() {
-  getCategories();
   return (
     <div className="App">
       <BrowserRouter>
