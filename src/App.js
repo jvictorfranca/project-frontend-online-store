75c0954f7ecf2aef538d6f43b7b4c2621b79ec48
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-10 16:35:18 -0300

    Requisito 7 sem aprovaçao pois falta requisito 6

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductsList from './pages/ProductsList';
 import CategoryList from './Components/CategoryList';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <BrowserRouter>
         <Route exact path="/" component={ ProductsList } />
         <Route path="/cart" component={ ShoppingCart } />
+        <Route path="/product/:id" render={ (props) => <ProductDetails { ...props } /> } />
       </BrowserRouter>
     </div>
   );
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends React.Component {
   render() {
     const { product } = this.props;
-    const { title, price, thumbnail } = product;
+    const { title, price, thumbnail, id } = product;
     return (
       <section data-testid="product">
         <h1>{title}</h1>
         <img src={ thumbnail } alt="Product Thumbnail" />
         <p>{price}</p>
+        <Link
+          to={
+            { pathname: `/product/${id}`,
+              state: { product } }
+          }
+          data-testid="product-detail-link"
+        >
+          More Info
+        </Link>
 
       </section>
     );
@@ -19,7 +29,8 @@ class ProductCard extends React.Component {
 ProductCard.propTypes = {
   product: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     thumbnail: PropTypes.string.isRequired }).isRequired,
 
 };
