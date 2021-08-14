5e5e726164d8ada9ef73011a011abacc2be8ae81
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-08-14 00:23:38 -0300

    Updates

---
+++ precache-manifest.83a716558c1fd8516e5ec525b786504c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fca29c0c52a351d08c89ded15ae8e60",
+    "url": "/project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "dbf53b059d0595a506bc",
+    "url": "/project-frontend-online-store/static/css/main.f79326df.chunk.css"
+  },
+  {
+    "revision": "873033814a763b6b7ee0",
+    "url": "/project-frontend-online-store/static/js/2.c92610c1.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/project-frontend-online-store/static/js/2.c92610c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf53b059d0595a506bc",
+    "url": "/project-frontend-online-store/static/js/main.239a5ec8.chunk.js"
+  },
+  {
+    "revision": "430674a48a6c1f3060ff",
+    "url": "/project-frontend-online-store/static/js/runtime-main.411a41b2.js"
+  }
+]);
